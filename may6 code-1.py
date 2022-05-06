d0a764b68fd0fadda8b7fd91755ac0fb903ae1d5
Vishnu-2302 <99253009+Vishnu-2302@users.noreply.github.com>
2022-05-06 16:46:06 +0530

    Add files via upload

---
+++ may6 code-1.py
@@ -0,0 +1,7 @@
+D={1:[1,'AJAY','MALE','BCA','PASS'],2:[2,'VISHNU','MALE','BCA','FAIL'],3:[3,'SUJAY','MALE','BCA','PASS']}
+n=int(input('Enter the Student Number: '))
+print('Student Number:',D[n][0])
+print('Student Name:',D[n][1])
+print('Student Gender:',D[n][2])
+print('Student Group:',D[n][3])
+print('Student Result:',D[n][4])
---
+++ may6 code-2.py
@@ -0,0 +1,7 @@
+D={1:[1,'AJAY','C++','06-05-2022','45000'],2:[2,'VISHNU','PYTHON','06-05-2022','FAIL'],3:[3,'SUJAY','JAVA','06-05-2022','45000']}
+n=int(input('Enter the Employee Number: '))
+print('Employee Number:',D[n][0])
+print('Employee Name:',D[n][1])
+print('Employee Design:',D[n][2])
+print('Employee Date Of Joining:',D[n][3])
+print('Employee Salary:',D[n][4])
